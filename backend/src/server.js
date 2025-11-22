109166fb585d9831b9a035a5f18f0ee5db737965
Maliha Nawshin Rahman <malihanawshin@gmail.com>
2025-11-22 12:22:11 +0100

    first deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,26 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.route.js';
+import path from 'path';
 
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
 const port = process.env.PORT || 3000;
 
 console.log(`PORT: ${process.env.PORT}`);
 
 app.use("/api/auth",authRoutes);
+
+//ready for production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+    
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+    });
+}
+
 
 app.listen(port, () =>  console.log(`Server is running on http://localhost:${port}`));
