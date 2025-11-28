48844efd741f409d98c304c82ed19e8cdda7b06d
Maliha Nawshin Rahman <malihanawshin@gmail.com>
2025-11-28 16:51:02 +0100

    socket real time message update

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -17,12 +17,12 @@ const io = new Server(server, {
 // apply authentication middleware to all socket connections
 io.use(socketAuthMiddleware);
 
-// we will use this function to check if the user is online or not
+// function to check if the user is online or not
 export function getReceiverSocketId(userId) {
   return userSocketMap[userId];
 }
 
-// this is for storig online users
+// this is for storing online users
 const userSocketMap = {}; // {userId:socketId}
 
 io.on("connection", (socket) => {
